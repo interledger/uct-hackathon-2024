69c8391d10f305b18533c14d896d79c56a3b21c6
takundachirema <takunda.chirema@alumni.uct.ac.za>
2024-06-07 14:48:33 +0200

    fix: fixed node-fetch and build issues
    manual node-fetch install and webpack bundler config in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,30 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
 await import("./src/env.js");
+import CopyPlugin from "copy-webpack-plugin";
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  webpack: (config) => {
+    // append the CopyPlugin to copy the file to your public dir
+    config.plugins.push(
+      new CopyPlugin({
+        patterns: [
+          {
+            from: "node_modules/@interledger/open-payments/dist/openapi/specs",
+            to: "server/vendor-chunks/specs",
+          },
+        ],
+      }),
+    );
+    // Important: return the modified config
+    return config;
+  },
+};
 
 export default config;
