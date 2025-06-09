5d74be9643c70f58a8e4c343abf033a27079578f
takundachirema <takunda.chirema@alumni.uct.ac.za>
2025-06-09 10:07:24 +0200

    feature: use docker for the app and the db

--- src/trpc/server.ts
+++ src/trpc/server.ts
@@ -5,6 +5,7 @@ import { cache } from "react";
 
 import { createCaller } from "$/src/server/api/root";
 import { createTRPCContext } from "$/src/server/api/trpc";
+import { NextRequest } from "next/server";
 
 /**
  * This wraps the `createTRPCContext` helper and provides the required context for the tRPC API when
@@ -15,7 +16,7 @@ const createContext = cache(() => {
   heads.set("x-trpc-source", "rsc");
 
   return createTRPCContext({
-    headers: heads,
+    req: new NextRequest(""),
   });
 });
 
